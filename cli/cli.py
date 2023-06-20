2e0ac0d60a98d9453f08fa061c2ceded7ba984d4
Nikola352 <nikolajolovic02@gmail.com>
2023-06-20 23:30:00 +0200

    Add search result highlighting

--- cli/cli.py
+++ cli/cli.py
@@ -1,7 +1,7 @@
 import time
 from entity.status import Status
 from ranking.edge_rank import edge_rank
-from ranking.search import search as post_search
+from ranking.search import search as post_search, parse_query
 from structures.graph import Graph
 
 
@@ -14,8 +14,27 @@ def login() -> str:
     return username
 
 
+def highlight_message(message: str, words: list[str], phrases: list[str]) -> str:
+    message_words = message.split()
+    reduced_message_words = ["".join(filter(str.isalnum, word)) for word in message_words] # remove non-alphanumeric characters
+    reduced_message_words = list(map(lambda word: word.lower(), reduced_message_words)) # to lower case
+
+    for word, reduced_word in zip(message_words, reduced_message_words):
+        if reduced_word in words:
+            highlighted_word = "\033[1;33m" + word + "\033[0m"
+            message_words[message_words.index(word)] = highlighted_word
+
+    message = " ".join(message_words)
+
+    for phrase in phrases:
+        if phrase in message:
+            message = message.replace(phrase, "\033[1;33m" + phrase + "\033[0m")
+
+    return message
+
+
 def show_feed(user: str, statuses: list[Status], affinity_graph: Graph):
-    print("Personalized feed")
+    print("\033[1;36mPERSONALIZED FEED\033[0m")
     print("---------------")
     print("Here are the most relevant posts in your feed:")
 
@@ -38,7 +57,7 @@ def show_feed(user: str, statuses: list[Status], affinity_graph: Graph):
 
 def search(user: str, statuses: list[Status], trie_map: dict[str, list[Status]], affinity_graph: Graph):
     while True:
-        print("Search")
+        print("\033[1;36mSEARCH\033[0m")
         print("---------------")
         print("Enter a keyword to search for in the feed, or type 'exit' to quit the program:")
         keyword = input()
@@ -49,11 +68,14 @@ def search(user: str, statuses: list[Status], trie_map: dict[str, list[Status]],
         print("---------------")
         print("Here are the most relevant posts for your search:")
 
+        words, phrases = parse_query(keyword)
+
         results = post_search(keyword, statuses, trie_map, user, affinity_graph)
 
         for result in results:
             print('====================')
-            print(result.status_message)
+            print(highlight_message(result.status_message, words, phrases))
+            # print(result.status_message)
             print("Type:", result.status_type)
             print("Link:", result.status_link)
             print("Published:", time.strftime("%d-%m-%Y %H:%M:%S", result.date_published))
